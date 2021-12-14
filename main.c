369f643faf657835682e2ed4ee64ab9e0ae611ef
Ariel Zidon <93542763+ArielZidon@users.noreply.github.com>
2021-12-15 01:17:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,15 +10,6 @@ int main(){
     scanf("%[^\n]s" , text);
 
     //gimatria
-    char word1[30];
-    char text1[1024];
-    gimatria(word,word1);
-    gimatria(text,text1);
-   
-    // test to see if i can push to git
-    
-
-    printf("%s",word1);
-    printf("%s",text1);
+ 
     
 }
